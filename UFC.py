7d56c0ce7091c05a096ad54ce863391522ebe5fb
BugraAKKUS <137571670+BugraAKKUS@users.noreply.github.com>
2025-01-11 19:55:05 +0300

    Add files via upload
    python quary check

---
+++ UFC.py
@@ -0,0 +1,17 @@
+import MySQL.connector
+
+mydb=mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="student",
+  database="UFC"
+)
+
+print(mydb
+
+mycursor=mydb.cursor()
+
+mycursor.execute("SELECT * FROM `FIGHTER` WHERE nationality='USA'")
+
+for x mycursor:
+  print(x)
